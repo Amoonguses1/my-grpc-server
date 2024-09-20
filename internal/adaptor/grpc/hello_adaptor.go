7f5a38a50919d4104fcac45866c8784e711dd9ee
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2024-09-20 14:26:58 +0900

    Study Unary and Server Stream

--- internal/adaptor/grpc/hello_adaptor.go
+++ internal/adaptor/grpc/hello_adaptor.go
@@ -2,6 +2,8 @@ package grpc
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/amoonguses1/grpc-proto-study/protogen/go/hello"
 )
@@ -12,4 +14,20 @@ func (a *GrpcAdaptor) SayHello(ctx context.Context, req *hello.HelloRequest) (*h
 	return &hello.HelloResponse{
 		Greet: greet,
 	}, nil
+}
+
+func (a *GrpcAdaptor) SayManyHellos(req *hello.HelloRequest, stream hello.HelloService_SayManyHellosServer) error {
+	for i := 0; i < 10; i++ {
+		greet := a.helloService.GenerateHello(req.Name)
+
+		res := fmt.Sprintf("[%d] %s", i, greet)
+		stream.Send(
+			&hello.HelloResponse{
+				Greet: res,
+			},
+		)
+
+		time.Sleep(500 * time.Millisecond)
+	}
+	return nil
 }
